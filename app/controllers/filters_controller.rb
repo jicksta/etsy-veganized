0eba4d02a30150e9414c0353eacb0dde931df496
Jay Phillips <jicksta+git@gmail.com>
2010-03-08 00:11:52 -0800

    Basic MongoDB filtering in filters_controller

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,5 +1,8 @@
 class FiltersController < ApplicationController
   def index
-    @products = Product.all.limit 200
+    @products = Product.all.
+                  limit(200).
+                  where(:materials.nin => %w[wool leather cashmere silk feather feathers]).
+                  where(:tags.nin => %w[baby dress blouse skirt])
   end
 end
